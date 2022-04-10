3f5b84836e39367d18d15bc14c2557adb369ecde
Borys <92867567+BorysSabov@users.noreply.github.com>
2022-04-10 13:03:16 +0300

    practic-mentor-10/04/22

--- js/practice.js
+++ js/practice.js
@@ -1,29 +1,29 @@
 // let - можно изменять 
 // cost-нельщя изменять
-let x = 6;
+// let x = 6;
 
-x = 7;
+// x = 7;
 
-const y = 8; 
+// const y = 8; 
 
 // console.log(x, y);
 
 // типы string number booleam null undefined
 
-let empty;
+// let empty;
 
 // console.log(typeof null);
 
 
 // string
 
-let str = 'HHH';
-str.toLowerCase();
+// let str = 'HHH';
+// str.toLowerCase();
 // console.log(str.toLowerCase());
 // console.log(str.length);
 
 //number
-let num = 10;
+// let num = 10;
 // console.log(num % 3);
 
 // boolean false-values| false | null | undefined | "" | 0 | NaN
@@ -40,12 +40,33 @@ let num = 10;
 
 
 // Логические операторы && -и до первого false||- или до первого true !-не
-console.log(true && 3);
+// console.log(true && 3);
 
-console.log(false && 3);
+// console.log(false && 3);
 
-console.log(true && 4 && "kiwi");
+// console.log(true && 4 && "kiwi");
 
-console.log(true && 0 && "kiwi");
+// console.log(true && 0 && "kiwi");
 
-console.log(true || 3);
+// console.log(true || 3);
+
+
+
+
+
+/////////////////////////////////////Занятие с ментором 
+// https://www.youtube.com/watch?v=Nv3Kn7TMMjk обьекты видео подробно
+
+// бэк для этой функции https://6251bfb67f7fa1b1ddde85d8.mockapi.io/api/contacts
+function updateNumber(array) {
+    const emptyArr = array
+        .filter(({ number }) => !number)
+        .map(contact => ({
+            ...contact,
+            number: '911',
+        }));
+
+    const arr = array.filter(({ number }) => number);
+
+    return [...emptyArr, ...arr];
+}
